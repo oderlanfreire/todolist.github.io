34e46be5fc4867d8e20b48e8fb5dfaf9252acf43
Oderlan <oderlanfreire@gmail.com>
2024-08-19 19:03:36 -0300

    modificação da dom, modificação em estilos e no html

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const drawer = document.getElementById('drawer')
 const saveTask = document.getElementById('save-task')
 const backdrop = document.getElementById('backdrop')
 const btnCloseDrawer = document.getElementById('ghost-button-close')
-const taskArea = document.getElementById('task-area')
+const taskArea = document.getElementById('container')
+const checkbox = document.getElementById('check')
 const tasks = []
-
 
 
 //funções de verificação
@@ -34,6 +34,10 @@ btnCreateTask.addEventListener('mouseenter', ()=>{
     btnCreateTask.src = `../components/img/crate-task-hover.svg`
 })
 
+btnCloseDrawer.addEventListener('mouseout', ()=>{
+    btnCreateTask.src = `../components/img/botão.svg`
+})
+
 btnCreateTask.addEventListener('mouseleave', ()=>{
     btnCreateTask.src = `../components/img/botão.svg`
 })
@@ -42,20 +46,48 @@ btnCloseDrawer.addEventListener('click', ()=>{
     drawer.classList.remove('open')
     backdrop.classList.remove('show')
     drawer.classList.add('closed')
-console.log(backdrop.classList)
-console.log(drawer.classList)
 })
 
 saveTask.addEventListener('click', ()=>{
-    const name = document.getElementById('task-name')
-    const desc = document.getElementById('task-description')
-
+    let name = document.getElementById('task-name')
+    let desc = document.getElementById('task-description')
+    
     if(isNullTask(name.value) === false){
         alert('Erro: Nome da tarefa em branco ou nula.')
+    }else{
+        let check = document.createElement('input')
+        check.id = 'check'
+        check.type = 'checkbox'
+
+        let label = document.createElement('label')
+        label.textContent = name.value
+
+        let description = document.createElement('span')
+        description.id='subcontent2'
+        description.innerText = desc.value
+        
+        let lblContainer = document.createElement('div')
+        lblContainer.className = 'container-lbl'
+        lblContainer.appendChild(check)
+        lblContainer.appendChild(label)
+        
+        let elmContainer = document.createElement('div')
+        elmContainer.className = 'container-elm'
+        elmContainer.appendChild(lblContainer)
+        elmContainer.appendChild(description)
+
+        tasks.push(elmContainer)
+
+        taskArea.innerHTML = ''
+        tasks.forEach(task =>{
+            taskArea.appendChild(task)
+            taskArea.appendChild(document.createElement('hr'))
+        })
+        name.value = ''
+        desc.value = ''
+        drawer.classList.add('closed')
     }
-    // }else{
-    //     // taskArea.
-    // }
+
 })
 
 
